16326dab153f88ceedc45e3003f79c7d29b027fd
guivicentep <guivicente.gvp@hotmail.com>
2020-07-10 13:37:42 -0300

    Commit 1007

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
 
-import { Header } from './styles';
+import { Header, RepositoryInfo, Issues } from './styles';
 
 interface RepositoryParams {
   repository: string;
@@ -10,7 +13,63 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <Header />;
-};
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then(response => {
+      console.log(response.data);
+    });
 
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      console.log(response.data);
+    });
+  }, [params.repository]);
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronLeft size={16} />
+          Voltar
+        </Link>
+      </Header>
+
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?v=4"
+            alt="rocketseat"
+          />
+          <div>
+            <strong>mobile/falaAmigo</strong>
+            <p>descrição do repositório</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Start</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Issues abertas</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+
+      <Issues>
+        <Link to="sasdsadsadas">
+          <div>
+            <strong>repositoryfull_name</strong>
+            <p>repositorydescription</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
+};
 export default Repository;
